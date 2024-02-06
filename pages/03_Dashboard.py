d0bc548743aa339732e7e35a3747bcd5bf402d6e
phyo swin <yophyoholland@gmail.com>
2024-02-06 12:49:11 +0800

    updated Dashboard.py

--- pages/03_Dashboard.py
+++ pages/03_Dashboard.py
@@ -42,46 +42,39 @@ this_year = today.year
 jan_1 = datetime.date(this_year, 1, 1)
 dec_31 = datetime.date(this_year, 12, 31)
 
-# Select date interval using st.date_input
-selected_date_range = st.sidebar.date_input(
-    "Select date interval",
-    (jan_1, dec_31),
-    jan_1,
-    dec_31,
-    format="YYYY/MM/DD",
-)
-
-start_date, end_date = selected_date_range
-start_date = pd.Timestamp(start_date)
-end_date = pd.Timestamp(end_date)
-filtered_entries = filter_entries(start_date, end_date)
-filtered_entries['Date'] = filtered_entries['Date'].dt.date
+# Separate start date and end date input fields
+start_date = st.sidebar.date_input("Select start date", jan_1, jan_1, dec_31, format="YYYY/MM/DD")
+end_date = st.sidebar.date_input("Select end date", dec_31, jan_1, dec_31, format="YYYY/MM/DD")
 
 try:
-    # Check if the start date is after the end date
-    if start_date > end_date:
-        st.markdown("**Incorrect date interval entered. Start date cannot be after end date.**", unsafe_allow_html=True)
-    else:
-        filtered_entries = filter_entries(start_date, end_date)
-        filtered_entries['Date'] = filtered_entries['Date'].dt.date
-
-        # Display entries if found
-        if not filtered_entries.empty:
-            # Iterate over DataFrame rows only once
-            for index, row in filtered_entries.iterrows():
-                # Access each column value
-                column1 = row["Date"]
-                column2 = row['Emotion']
-                column3 = row['Reason for emotion']
-
-                # Create a box for each row
-                with st.container(height=150, border=True):
-                    st.markdown(f"##### **:blue[{column1}]**")
-                    st.markdown(f"**Emotion:** {column2}")
-                    st.markdown(f"**What happened:** {column3}")
-
+    # Check if both start date and end date are selected
+    if start_date is not None and end_date is not None:
+        # Check if the start date is after the end date
+        if start_date > end_date:
+            st.markdown("**Incorrect date interval entered. Start date cannot be after end date.**", unsafe_allow_html=True)
         else:
-            st.markdown("**No entries found for the selected date range.**", unsafe_allow_html=True)
+            filtered_entries = filter_entries(start_date, end_date)
+            filtered_entries['Date'] = filtered_entries['Date'].dt.date
+
+            # Display entries if found
+            if not filtered_entries.empty:
+                # Iterate over DataFrame rows only once
+                for index, row in filtered_entries.iterrows():
+                    # Access each column value
+                    column1 = row["Date"]
+                    column2 = row['Emotion']
+                    column3 = row['Reason for emotion']
+
+                    # Create a box for each row
+                    with st.container(height=150, border=True):
+                        st.markdown(f"##### **:blue[{column1}]**")
+                        st.markdown(f"**Emotion:** {column2}")
+                        st.markdown(f"**What happened:** {column3}")
+
+            else:
+                st.markdown("**No entries found for the selected date range.**", unsafe_allow_html=True)
+    else:
+        st.markdown("**Please input both start and end dates.**", unsafe_allow_html=True)
 
 except Exception as e:
     # Handle any other exceptions
